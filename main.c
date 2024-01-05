9c651648bc1e4f7e08fb94e0f022d185d8bfd47c
Mister1SI <wazzup2690@gmail.com>
2024-01-04 21:01:02 -0800

    Made it work basic

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 #include <sys/types.h>
 #include <stdlib.h>
 #include <sys/file.h>
-
+#include <sys/stat.h>
 
 int main() {
 	const char* filename_prompt = "Enter the filename\n"; //19
@@ -12,32 +12,34 @@ int main() {
 	char filename_buf[512] = {0};
 	char new_filename_buf[512] = {0};
 
-	// allocate way too much memory (4MB)
-	char* filebuf = calloc(4194304, 1);
-
 	write(1, filename_prompt, 19);
 	read(0, filename_buf, 512);
+	write(1, new_filename_prompt, 23);
+	read(0, new_filename_buf, 512);
 
 	// remove the newlines
 	filename_buf[strcspn(filename_buf, "\n")] = 0;
 	new_filename_buf[strcspn(new_filename_buf, "\n")] = 0;
 
-	int fd = open(filename_buf, O_RDONLY);
+	int fd = open(filename_buf, O_RDONLY, 00644);
 	if(fd == -1) {
 		char* msg = "Failed to open the file\n";
 		write(1, msg, 24);
 		return 1;
 	}
 
-	read(fd, filebuf, 4194304);
-	int newfd = open(new_filename_buf, O_CREAT | O_WRONLY);
-	write(newfd, filebuf, 4194304);
+	struct stat st;
+	fstat(fd, &st);
+	long filesize = (long) st.st_size;
+
+	char* filebuf = calloc(filesize, 1);
+
+	read(fd, filebuf, filesize);
+	int newfd = open(new_filename_buf, O_CREAT | O_WRONLY, 00644);
+	write(newfd, filebuf, filesize);
 
 	close(fd);
 	close(newfd);
-
-	write(1, filename_buf, 512);
-	write(1, new_filename_buf, 512);
 
 	return 0;
 }
